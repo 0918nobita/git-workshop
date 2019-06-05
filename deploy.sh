ac8a59a15df098b6287ba21b401ea9b6e2cc690f
0918nobita <nobita.0918@gmail.com>
2019-06-05 18:53:48 +0900

    :wrench: Regenerate deploy_key.enc

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ rm -f deploy_key deploy_key.enc deploy.sh .travis.yml package.json
 git init
 git add .
 git commit -m "Publishing site on `date "+%Y-%m-%d %H:%M:%S"`"
-git push -f git@github.com:0918nobita/forth-book.git master:gh-pages
+git push -f git@github.com:0918nobita/git-workshop.git master:gh-pages
